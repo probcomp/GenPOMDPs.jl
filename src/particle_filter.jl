33fb4bccb7618adca262696eee5837a6855bbaa6
George Matheos <georgematheos@gmail.com>
2023-06-05 12:48:00 -0400

    remove scratch work

--- src/particle_filter.jl
+++ src/particle_filter.jl
@@ -47,24 +47,6 @@ function pf_observable(
     # Initialize
     obs0 = only(observations_and_actions[][1])
     pf_states = Observables.Observable([ initialize_pf(obs0) ])
-    
-    _pending_updates = []
-    _processing = false
-    function process_updates!()
-        _processing = true
-
-        (obs, act) = deleteat!(_pending_updates, 1)
-
-        pf_state = pf_states[][end]
-        updated = update_pf(pf_state, actions[end], obss[end])
-        pf_states[] = push!(pf_states[], updated)
-
-        if length(_pending_updates) > 0
-            process_updates!()
-        else
-            _processing = false
-        end
-    end
 
     # Whenever a new observation and action become available:
     Observables.on(observations_and_actions) do (obss, actions)
@@ -78,8 +60,6 @@ function pf_observable(
         pf_state = pf_states[][end]
         updated = update_pf(pf_state, actions[end], obss[end])
         pf_states[] = push!(pf_states[], updated)
-        # push!(_pending_updates, (obss, actions))
-        # @async process_updates!()
     end
 
     return pf_states
